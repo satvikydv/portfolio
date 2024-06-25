23dd70164f60f6b7618e1b0fb68b3d4923705b03
Satvik Yadav <satvikydv@gmail.com>
2024-06-25 20:44:24 +0530

    Home page update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,19 @@
+import {Link} from 'react-router-dom';
+import './index.scss';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <>
+    <div className="container home-page">
+        <div className="text-zone">
+            <h1>Hi, <br /> I'm Satvik
+            <br /> Web Developer
+            </h1>
+            <h2>Java / React / JavaScript</h2>
+            <Link to="/contact" className="flat-button">Contact Me</Link>
+        </div>
     </div>
+    </>
   );
 }
 

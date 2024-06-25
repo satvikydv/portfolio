cf56ac789b1504a83d3e2674e5effa1537a61146
Satvik Yadav <satvikydv@gmail.com>
2024-06-25 13:12:44 +0530

    side panel

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,23 @@
- import Sidebar from '../Sidebar';
-import './index.scss';
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar'
+import './index.scss'
 
- const Layout = () => {
-    return (
-      <div>
-         <h1><Sidebar /></h1>
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
       </div>
-    );
+    </div>
+  )
 }
 
-export default Layout;
+export default Layout
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,7 @@ import {Link, NavLink} from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
    return (
@@ -13,15 +14,35 @@ const Sidebar = () => {
 
          <nav>
             <NavLink exact="true" to='/' activeClassName='active'>
-                <FontAwesomeIcon icon={faHome} color="grey" />
+                <FontAwesomeIcon icon={faHome} color="white" />
             </NavLink>
-            <NavLink exact="true" to='/' activeClassName='active' className="about-link">
-                <FontAwesomeIcon icon={faUser} color="grey" />
+            <NavLink exact="true" to='/about' activeClassName='active' className="about-link">
+                <FontAwesomeIcon icon={faUser} color="white" />
             </NavLink>
-            <NavLink exact="true" to='/' activeClassName='active' className="contact-link">
-                <FontAwesomeIcon icon={faEnvelope} color="grey" />
+            <NavLink exact="true" to='/contact' activeClassName='active' className="contact-link">
+                <FontAwesomeIcon icon={faEnvelope} color="white" />
             </NavLink>
          </nav>
+
+         <ul>
+            <li>
+                <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/satvikydv/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="grey" />
+                </a>
+            </li>
+
+            <li>
+                <a target='_blank' rel="noreferrer" href='https://github.com/satvikydv'>
+                    <FontAwesomeIcon icon={faGithub} color="grey" />
+                </a>
+            </li>
+
+            <li>
+                <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/satvikydv/'>
+                    <FontAwesomeIcon icon={faTwitter} color="grey" />
+                </a>
+            </li>
+         </ul>
       </div>
    );
 }

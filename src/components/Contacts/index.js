c710b3966701b1baba5bc61f8bbcf6764f9c3b3f
Satvik Yadav <satvikydv@gmail.com>
2024-07-16 02:04:46 +0530

    leaflet map on contact page

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -3,6 +3,8 @@ import Loader from 'react-loaders'
 import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -87,6 +89,25 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Satvik Yadav
+          <br />
+          IIIT Naya Raipur
+          <br />
+          India
+          <br />
+          <span>satvikydv@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[21.12840433503101, 81.7661653952704]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={[21.12840433503101, 81.7661653952704]}>
+            <Popup>
+              Satvik lives here!
+            </Popup>
+          </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type='ball-pulse' active />
